4baa0f39887ff4f8bc4a24131021fb3c7f778c29
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-10-12 15:37:26 +0800

    put everything in prototypes with a functioning board

--- javascript.js
+++ javascript.js
@@ -1,101 +1,138 @@
-const players = {
-    player1:"X",
-    player2:"O"
-}
+function GameController() {    
+    const board = GameBoard() 
+    const turn = Turn()
+    const player = Players()
 
-let gameboard = {
-    tile0:"",
-    tile1:"",
-    tile2:"",
-    tile3:"",
-    tile4:"",
-    tile5:"",
-    tile6:"",
-    tile7:"",
-    tile8:"",
-}
-
-let winningCombinations = [
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,4,8],
-    [2,4,6]
-]
-
-playGame = (players) => {
-    
-    let currentPlayer = ""
-    makeMove = (player,gameboard,tile) => {
-        return gameboard[tile] = player
-    }
-    // current player switch
-    for (let turn = 0; turn <= 9; turn++){
-        if (turn % 2 === 0) {
-            currentPlayer = players.player1
-        } else {
-            currentPlayer = players.player2
-        }
+    const tiles = document.querySelectorAll(".place-tile")
+    for (let i = 0; i < tiles.length; i++){
+        tiles[i].addEventListener("click", () => {
         
-        let tileMove = prompt("Next Move")
-        makeMove(currentPlayer,gameboard,tileMove)
-        console.log(gameboard)
-    }   
+            // set value if clicked
+            if (tiles[i].textContent === ""){
+                board.setValue(i,player.getPlayer())
+                turn.addTurn()
+                player.changePlayer()
+                board.updateBoard()
+            } else {
+                console.log("invalid move")
+            }
+        })
+    }
+
 }
 
-playGame(players)
+checkWin = (gameboard, selectedPlayer) => {
+    let winningCombinations = [
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6]
+    ]
 
-// createTiles = (gameboard) => {
-//     const container = document.querySelector("#container")
+    for(let i = 0; i< winningCombinations.length ;i++) {
+        console.log(winningCombinations[i])
+        for(let n = 0; n<= 3 ;n++) {
+            if (n === 3) {
+                return true
+            }
+            const selected = winningCombinations[i][n]
+            if (gameboard[selected] === selectedPlayer){
+                continue
+            } else {
+                break
+            }
+        }
+    }
 
-//     for (let i = 0; i < 9; i++) {
-//         const tile = document.createElement("div")
-//         tile.classList.add("place-tile")
-//         tile.id = "tile" + i
-//         tile.addEventListener("click", () => {
-//             gameboard[tile0] = "X"
-//         })
-//         container.appendChild(tile);
-//     }
-// }
+    return false
+}
+
+// console.log(checkWin(gameboard,"X"))
+
+// playGame(players)
+
+function Players() {
+    const players = {
+        player1:"X",
+        player2:"O"
+    }
+        
+    let currentPlayer = players.player1
+
+    const getPlayer = () => currentPlayer
+
+    const changePlayer = () => {
+        if (currentPlayer === players.player1) {
+            currentPlayer = players.player2
+        } else {
+            currentPlayer = players.player1
+        }
+    }
+
+    return {
+        getPlayer,
+        changePlayer
+    }
+}
+
+function Turn() {
+    let turn = 0
+
+    const getTurn = () => turn
+
+    const addTurn = () => {
+        turn++
+        console.log('turn ' + turn)
+    }
+
+
+    return {
+        getTurn,
+        addTurn
+    }
+}
+
+function GameBoard() {
+    let board = ["","","","","","","","",""]
+
+    const container = document.querySelector("#container")
+    
+
+    // board creation 9 tiles
+    for (let i = 0; i < 9; i++) {
+        const tile = document.createElement("div")
+        tile.classList.add("place-tile")
+        tile.id = "tile" + i
+        container.appendChild(tile);
+    }
+    
+    // get the gameBoard
+    const getBoard = () => board
+
+    const setValue = (pos,player) => {
+        board[pos] = player
+    }
+
+    //gets the board list and updates the board
+    const updateBoard = () => {
+        const tiles = document.querySelectorAll(".place-tile")
+        for (let i = 0; i < tiles.length; i++) {
+            tiles[i].textContent = board[i]
+        }
+    }
+
+    return {
+        getBoard, 
+        updateBoard,
+        setValue
+    }
+}
 
 
 
-// playGame = (players, gameboard) => {
-//     let turn = 0
-//     createTiles()
-//     console.log(gameboard)
-
-//     for (let i = 0; i <= winningCombinations.length; i++){
-//         console.log(i)
-//         turn++
-//     }  
-// }
-
-// playGame(players, gameboard)
-
-
-
-
-// checkBoard = (currentBoard) => {
-//     for (win in winningCombinations) {
-//         let winCopy = win
-//         console.log(winCopy)
-//         for (item in currentBoard) {
-//             if (winCopy.includes(item)){
-//                 const index = winCopy.indexOf(item)
-//                 if (index !== -1) {
-//                     winCopy.splice(index, 1);
-//                     console.log(winCopy)
-//                 }
-//             }
-//         }
-//     }
-// }
-
-
-// checkBoard([0,3,6])
+const board = GameController()
 
